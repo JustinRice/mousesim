2bfdf5db9aa01da7ea24b359458e0c01f30f7a1e
Justin Rice <jrice5@mail.umw.edu>
2016-09-29 15:49:51 -0400

    Parse.py coming into shape

--- mice.py
+++ mice.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
 
+from parse import Parser
 
 #----- Main
 print('This is a test!')
+parser = Parser("sampledata.csv")
+
+print parser.getData()
+
+for key in parser.getData().keys():
+    print(key)
+
